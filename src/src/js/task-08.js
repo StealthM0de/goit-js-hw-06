e4a05e2311131aeeb7627a765868f926166c8b6d
StealthM0de <jedongfallorina@gmail.com>
2023-11-10 22:34:48 +0800

    task 8

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,21 @@
+const logInForm = document.querySelector(".login-form");
 
+logInForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const emailInput = logInForm.elements.email;
+  const passWordInput = logInForm.elements.password;
+
+  if (!emailInput.value || !passwordInput) {
+    alert("All fields must be filled in.");
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    console.log(formData);
+
+    logInForm.reset();
+  }
+});
